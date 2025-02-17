c11a8006685684d11ccf9e5eb444ee5038fb612e
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-02-17 20:15:20 +0100

    Added json tags to generated sqlc models

--- types/types.go
+++ types/types.go
@@ -5,7 +5,7 @@ import (
 )
 
 type UserProfile struct {
-    User repository.User
-    UserColor string
-    UserGroup string
+	User      repository.User `json:"user"`
+	UserColor string          `json:"user_color"`
+	UserGroup string          `json:"user_group"`
 }
