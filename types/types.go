daa3338457213a1a686e67abaff38857a1af1bd4
Piquel <63727792+PiquelChips@users.noreply.github.com>
2025-02-18 15:16:19 +0100

    Sessions: created UserSession type to store token information

--- types/types.go
+++ types/types.go
@@ -1,7 +1,10 @@
 package types
 
 import (
+	"time"
+
 	repository "github.com/PiquelChips/piquel.fr/database/generated"
+	"github.com/markbates/goth"
 )
 
 type UserProfile struct {
@@ -9,4 +12,22 @@ type UserProfile struct {
 	Color     string `json:"color"`
 	Group     string `json:"group"`
 	GroupName string `json:"group_name"`
+}
+
+type UserSession struct {
+	AccessToken       string    `json:"access_token"`
+	AccessTokenSecret string    `json:"access_token_secret"`
+	RefreshToken      string    `json:"refresh_token"`
+	ExpiresAt         time.Time `json:"expires_at"`
+	IDToken           string    `json:"id_token"`
+}
+
+func UserSessionFromGothUser(user *goth.User) *UserSession {
+	return &UserSession{
+		AccessToken:       user.AccessToken,
+		AccessTokenSecret: user.AccessTokenSecret,
+		RefreshToken:      user.RefreshToken,
+		ExpiresAt:         user.ExpiresAt,
+		IDToken:           user.IDToken,
+	}
 }
