2a6d594d3ee3881cb684522a2c79a42b8c14b15a
Piquel <63727792+PiquelChips@users.noreply.github.com>
2025-02-18 14:20:33 +0100

    Moved the GetProfile function

--- handlers/profile.go
+++ handlers/profile.go
@@ -5,43 +5,34 @@ import (
 	"net/http"
 
 	"github.com/PiquelChips/piquel.fr/services/database"
+	"github.com/PiquelChips/piquel.fr/services/users"
 	"github.com/PiquelChips/piquel.fr/types"
 	"github.com/gorilla/mux"
 	"github.com/jackc/pgx/v5"
 )
 
 func HandleProfileQuery(w http.ResponseWriter, r *http.Request) {
-    // Get username from query params. Should look likes "GET api.piquel.fr/profile?[username]
-    username := r.URL.Query().Get("profile")
-    writeProfile(w, r, username)
+	// Get username from query params. Should look likes "GET api.piquel.fr/profile?[username]
+	username := r.URL.Query().Get("profile")
+	writeProfile(w, r, username)
 }
 
 func HandleProfile(w http.ResponseWriter, r *http.Request) {
 	username := mux.Vars(r)["profile"]
-    writeProfile(w, r, username)
+	writeProfile(w, r, username)
 }
 
 func writeProfile(w http.ResponseWriter, r *http.Request, username string) {
-	user, err := database.Queries.GetUserByUsername(r.Context(), username)
+	profile, err := users.GetProfile(username)
 	if err != nil {
 		if err == pgx.ErrNoRows {
-            // Properly redirect to cookied URL
+			// Properly redirect to cookied URL
 			http.Redirect(w, r, "/", http.StatusNotFound)
 			return
 		}
-	}
-
-	profile := &types.UserProfile{User: user}
-
-	group, err := database.Queries.GetGroupInfo(r.Context(), user.Group)
-	if err != nil {
 		panic(err)
 	}
 
-	profile.Color = group.Color
-    profile.Group = group.Name
-	profile.GroupName = group.Displayname.String
-
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(profile)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(profile)
 }
--- services/users/users.go
+++ services/users/users.go
@@ -6,40 +6,61 @@ import (
 
 	repository "github.com/PiquelChips/piquel.fr/database/generated"
 	"github.com/PiquelChips/piquel.fr/services/database"
+	"github.com/PiquelChips/piquel.fr/types"
 	"github.com/PiquelChips/piquel.fr/utils"
 	"github.com/jackc/pgx/v5"
 	"github.com/markbates/goth"
 )
 
 func VerifyUser(context context.Context, inUser *goth.User) {
-    _, err := database.Queries.GetUserByEmail(context, inUser.Email)
-    if err != nil {
-        if err == pgx.ErrNoRows {
-            registerUser(context, inUser)
-            return
-        }
-        panic(err)
-    }
+	_, err := database.Queries.GetUserByEmail(context, inUser.Email)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			registerUser(context, inUser)
+			return
+		}
+		panic(err)
+	}
 }
 
 func registerUser(context context.Context, inUser *goth.User) {
-    params := repository.AddUserParams{}
+	params := repository.AddUserParams{}
 
-    params.Email = inUser.Email
-    params.Group = "default"
-    params.Image = inUser.AvatarURL
-    params.Created = time.Now()
-    params.Name = inUser.Name
+	params.Email = inUser.Email
+	params.Group = "default"
+	params.Image = inUser.AvatarURL
+	params.Created = time.Now()
+	params.Name = inUser.Name
 
-    switch inUser.Provider {
-    case "google":
-        params.Username = utils.FormatUsername(inUser.Name)
-    case "github":
-        params.Username = utils.FormatUsername(inUser.NickName)
-    }
+	switch inUser.Provider {
+	case "google":
+		params.Username = utils.FormatUsername(inUser.Name)
+	case "github":
+		params.Username = utils.FormatUsername(inUser.NickName)
+	}
 
-    err := database.Queries.AddUser(context, params)
-    if err != nil {
-        panic(err)
-    }
+	err := database.Queries.AddUser(context, params)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func GetProfile(username string) (*types.UserProfile, error) {
+	user, err := database.Queries.GetUserByUsername(context.Background(), username)
+	if err != nil {
+		return nil, err
+	}
+
+	profile := &types.UserProfile{User: user}
+
+	group, err := database.Queries.GetGroupInfo(context.Background(), user.Group)
+	if err != nil {
+		return nil, err
+	}
+
+	profile.Color = group.Color
+	profile.Group = group.Name
+	profile.GroupName = group.Displayname.String
+
+	return profile, nil
 }
