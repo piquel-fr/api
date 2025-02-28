3c6dc0d7d9c37c54f16d1bc7c32c317a8bee4f11
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-02-28 16:39:34 +0100

    Profile: Added log if user that does not exist is requested

--- handlers/profile.go
+++ handlers/profile.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/PiquelChips/piquel.fr/services/auth"
@@ -13,14 +14,14 @@ import (
 func HandleProfileQuery(w http.ResponseWriter, r *http.Request) {
 	// Get username from query params. Should look likes "GET api.piquel.fr/profile?[username]
 	username := r.URL.Query().Get("profile")
-    if username == "" {
-        var err error
-        username, err = auth.GetUsername(r)
-        if username == "" || err != nil {
-            http.Error(w, "You are not logged in", http.StatusUnauthorized)
-            return
-        }
-    }
+	if username == "" {
+		var err error
+		username, err = auth.GetUsername(r)
+		if username == "" || err != nil {
+			http.Error(w, "You are not logged in", http.StatusUnauthorized)
+			return
+		}
+	}
 	writeProfile(w, r, username)
 }
 
@@ -33,6 +34,7 @@ func writeProfile(w http.ResponseWriter, r *http.Request, username string) {
 	profile, err := users.GetProfile(username)
 	if err != nil {
 		if err == pgx.ErrNoRows {
+            log.Printf("User %s does not exist!", username)
 			// Properly redirect to cookied URL
 			http.Redirect(w, r, "/", http.StatusNotFound)
 			return
