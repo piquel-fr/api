a96e252f7466e430f89a23eb8a14c6ad0d0e9b34
Piquel <63727792+PiquelChips@users.noreply.github.com>
2025-05-16 10:21:13 +0200

    run go fmt

--- main.go
+++ main.go
@@ -17,28 +17,28 @@ import (
 func main() {
 	log.Printf("Initializing piquel.fr API...\n")
 
-    // Intialize services
-    config.LoadConfig()
-    auth.InitAuthentication()
-    auth.InitCookieStore()
-    database.InitDatabase()
-    defer database.DeinitDatabase()
-    types.Init()
+	// Intialize services
+	config.LoadConfig()
+	auth.InitAuthentication()
+	auth.InitCookieStore()
+	database.InitDatabase()
+	defer database.DeinitDatabase()
+	types.Init()
 
-    // Initialize the router
+	// Initialize the router
 	router := mux.NewRouter()
 	middleware.Setup(router)
 
 	log.Printf("[Router] Initialized router!\n")
 
-    router.HandleFunc("/profile", handlers.HandleBaseProfile).Methods(http.MethodGet, http.MethodPost, http.MethodOptions)
-    router.HandleFunc("/profile/{profile}", handlers.HandleProfile).Methods(http.MethodGet, http.MethodPost, http.MethodOptions)
+	router.HandleFunc("/profile", handlers.HandleBaseProfile).Methods(http.MethodGet, http.MethodPost, http.MethodOptions)
+	router.HandleFunc("/profile/{profile}", handlers.HandleProfile).Methods(http.MethodGet, http.MethodPost, http.MethodOptions)
 
 	router.HandleFunc("/auth/logout", handlers.HandleLogout).Methods(http.MethodGet, http.MethodOptions)
 	router.HandleFunc("/auth/{provider}", handlers.HandleProviderLogin).Methods(http.MethodGet, http.MethodOptions)
 	router.HandleFunc("/auth/{provider}/callback", handlers.HandleAuthCallback).Methods(http.MethodGet, http.MethodOptions)
 
-    address := fmt.Sprintf("0.0.0.0:%s", config.Envs.Port)
+	address := fmt.Sprintf("0.0.0.0:%s", config.Envs.Port)
 
 	log.Printf("[Router] Starting router...\n")
 	log.Printf("[Router] Listening on %s!\n", address)
