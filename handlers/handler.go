67e758fd3702c64b2d06494ed4c1ab61da7acb96
PiquelChips <piquel@piquel.fr>
2025-11-03 17:58:52 +0100

    setup root handler

--- handlers/handler.go
+++ handlers/handler.go
@@ -14,9 +14,16 @@ type Handler struct {
 
 func (h *Handler) CreateHttpHandler() http.Handler {
 	router := http.NewServeMux()
+	router.HandleFunc("/", h.rootHandler)
 	router.Handle("/auth/", http.StripPrefix("/auth", h.CreateAuthHandler()))
 	router.Handle("/profile/", http.StripPrefix("/profile", h.CreateProfileHandler()))
 	router.Handle("/docs/", http.StripPrefix("/docs", h.CreateDocsHandler()))
 
 	return router
+}
+
+func (h *Handler) rootHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/html")
+	w.Write([]byte("Welcome to the Piquel API! Visit the <a href=\"https://piquel.fr/docs\">API</a> for more information."))
+	w.WriteHeader(http.StatusOK)
 }
