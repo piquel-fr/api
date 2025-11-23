84e1516e9670ca7613652619ae5f49e89a30ece8
PiquelChips <piquel@piquel.fr>
2025-11-23 20:41:51 +0100

    start implementing email stuff

--- go.mod
+++ go.mod
@@ -4,16 +4,20 @@ go 1.24.4
 
 require (
 	github.com/alecthomas/chroma/v2 v2.20.0
+	github.com/emersion/go-imap/v2 v2.0.0-beta.7
 	github.com/golang-jwt/jwt/v5 v5.3.0
 	github.com/gomarkdown/markdown v0.0.0-20250810172220-2e2c11897d1a
 	github.com/google/go-github/v74 v74.0.0
 	github.com/jackc/pgx/v5 v5.7.5
 	github.com/joho/godotenv v1.5.1
+	github.com/wneessen/go-mail v0.7.2
 	golang.org/x/oauth2 v0.27.0
 )
 
 require (
 	github.com/dlclark/regexp2 v1.11.5 // indirect
+	github.com/emersion/go-message v0.18.1 // indirect
+	github.com/emersion/go-sasl v0.0.0-20231106173351-e73c9f7bad43 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
--- handlers/email.go
+++ handlers/email.go
@@ -1,8 +1,107 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/piquel-fr/api/database"
+	"github.com/piquel-fr/api/services/auth"
+	"github.com/piquel-fr/api/utils/errors"
+)
 
 func (h *Handler) CreateEmailHandler() http.Handler {
 	handler := http.NewServeMux()
 	return handler
 }
+
+func (h *Handler) handleListEmails(w http.ResponseWriter, r *http.Request) {
+	requester, err := h.AuthService.GetUserFromRequest(r)
+	if err != nil {
+		errors.HandleError(w, r, err)
+		return
+	}
+
+	limitStr := r.URL.Query().Get("limit")
+	if limitStr == "" {
+		limitStr = "10"
+	}
+
+	limit, err := strconv.Atoi(limitStr)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("invalid number %s specified for limit", limitStr), http.StatusBadRequest)
+		return
+	}
+
+	if limit > 200 {
+		limit = 200
+	}
+
+	offsetStr := r.URL.Query().Get("offset")
+	if offsetStr == "" {
+		offsetStr = "0"
+	}
+
+	offset, err := strconv.Atoi(offsetStr)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("invalid number %s specified for offset", limitStr), http.StatusBadRequest)
+		return
+	}
+
+	email := r.URL.Query().Get("email")
+	if email == "" {
+		http.Error(w, fmt.Sprintf("please specify an email to get mail from"), http.StatusBadRequest)
+		return
+	}
+
+	account, err := database.Queries.GetMailAccountByEmail(r.Context(), email)
+	if err != nil {
+		errors.HandleError(w, r, err)
+		return
+	}
+
+	authRequest := &auth.Request{
+		User:      requester,
+		Ressource: &account,
+		Actions:   []string{"fetch"},
+		Context:   r.Context(),
+	}
+
+	if err = h.AuthService.Authorize(authRequest); err != nil {
+		errors.HandleError(w, r, err)
+		return
+	}
+
+	if r.URL.Query().Has("count") {
+		count, err := h.EmailService.CountEmailsForAccount(&account)
+		if err != nil {
+			errors.HandleError(w, r, err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/plain")
+		w.Write([]byte(strconv.Itoa(int(count))))
+		return
+	}
+
+	emails, err := h.EmailService.GetEmailsForAccount(&account, offset, limit)
+	if err != nil {
+		errors.HandleError(w, r, err)
+		return
+	}
+
+	data, err := json.Marshal(emails)
+	if err != nil {
+		errors.HandleError(w, r, err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
+}
+
+func (h *Handler) handleListEmailAccounts(w http.ResponseWriter, r *http.Request) {}
+
+func (h *Handler) handleAddAccount(w http.ResponseWriter, r *http.Request)    {}
+func (h *Handler) handleRemoveAccount(w http.ResponseWriter, r *http.Request) {}
