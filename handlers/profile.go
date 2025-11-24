85e9581bb1be46e9faf41a282c0c511279d9da64
Piquel <piquel@piquel.fr>
2025-11-24 15:20:13 +0100

    Fix fetching profile

--- handlers/profile.go
+++ handlers/profile.go
@@ -29,7 +29,7 @@ func (h *Handler) handleGetProfile(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handler) handleGetProfileQuery(w http.ResponseWriter, r *http.Request) {
-	username := r.URL.Query().Get("username")
+	username := r.URL.Query().Get("user")
 	if username == "" {
 		id, err := h.AuthService.GetUserId(r)
 		if err != nil {
