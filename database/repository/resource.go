0c3244dc54f9cf9c4e5e3da28a91d5d6dae53ade
PiquelChips <piquel@piquel.fr>
2025-11-27 20:40:30 +0100

    add constants to describe resource names to avoid typos

--- database/repository/resource.go
+++ database/repository/resource.go
@@ -1,10 +1,16 @@
 package repository
 
-func (profile *User) GetResourceName() string { return "user" }
+const (
+	ResourceUser         string = "user"
+	ResourceDocsInstance string = "docs_instance"
+	ResourceMailAccount  string = "email_account"
+)
+
+func (profile *User) GetResourceName() string { return ResourceUser }
 func (profile *User) GetOwner() int32         { return profile.ID }
 
-func (docs *DocsInstance) GetResourceName() string { return "docs_instance" }
+func (docs *DocsInstance) GetResourceName() string { return ResourceDocsInstance }
 func (docs *DocsInstance) GetOwner() int32         { return docs.OwnerId }
 
-func (account *MailAccount) GetResourceName() string { return "email_account" }
+func (account *MailAccount) GetResourceName() string { return ResourceMailAccount }
 func (account *MailAccount) GetOwner() int32         { return account.OwnerId }
