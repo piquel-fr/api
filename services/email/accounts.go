5edfc426cdaee53e8aa6addf82ac6dc6d573c32d
PiquelChips <piquel@piquel.fr>
2025-11-27 21:32:21 +0100

    implemented email service functions

--- services/email/accounts.go
+++ services/email/accounts.go
@@ -3,45 +3,28 @@ package email
 import (
 	"context"
 
+	"github.com/emersion/go-imap/v2/imapclient"
 	"github.com/piquel-fr/api/database"
 	"github.com/piquel-fr/api/database/repository"
 )
 
-type MailAccount struct {
-	Unread int `json:"unread"`
-	repository.MailAccount
+type Mailbox struct {
+	Name        string
+	NumMessages int `json:"num_messages"`
+	NumUnread   int `json:"num_unread"`
 }
 
 type AccountInfo struct {
-	MailAccount
+	*repository.MailAccount
+	Mailboxes []Mailbox
 }
 
 func (r *realEmailService) GetAccountByEmail(ctx context.Context, email string) (repository.MailAccount, error) {
 	return database.Queries.GetMailAccountByEmail(ctx, email)
 }
 
-func (r *realEmailService) ListAccounts(ctx context.Context, userId int32) ([]MailAccount, error) {
-	dbAccounts, err := database.Queries.ListUserMailAccounts(ctx, userId)
-	if err != nil {
-		return nil, err
-	}
-
-	accounts := []MailAccount{}
-	for _, dbAccount := range dbAccounts {
-		// TODO: get number of unreal emails
-
-		account := MailAccount{
-			MailAccount: dbAccount,
-			Unread:      0,
-		}
-
-		// make sure we don't return username and password
-		account.Username = ""
-		account.Password = ""
-		accounts = append(accounts, account)
-	}
-
-	return accounts, nil
+func (r *realEmailService) ListAccounts(ctx context.Context, userId int32) ([]repository.MailAccount, error) {
+	return database.Queries.ListUserMailAccounts(ctx, userId)
 }
 
 func (r *realEmailService) AddAccount(ctx context.Context, params repository.AddEmailAccountParams) (int32, error) {
@@ -53,5 +36,35 @@ func (r *realEmailService) RemoveAccount(ctx context.Context, accountId int32) e
 }
 
 func (r *realEmailService) GetAccountInfo(ctx context.Context, account *repository.MailAccount) (AccountInfo, error) {
-	return AccountInfo{}, nil
+	client, err := imapclient.DialTLS(r.imapAddr, nil)
+	if err != nil {
+		return AccountInfo{}, err
+	}
+	defer client.Logout()
+
+	if err := client.Login(account.Username, account.Password).Wait(); err != nil {
+		return AccountInfo{}, nil
+	}
+
+	accountInfo := AccountInfo{
+		MailAccount: account,
+	}
+
+	// don't want to send sensitive data to user, for internal use only
+	account.Username = ""
+	account.Password = ""
+
+	// get mailboxes
+	listCmd := client.List("", "*", nil)
+	defer listCmd.Close()
+
+	for mailbox := listCmd.Next(); mailbox != nil; mailbox = listCmd.Next() {
+		accountInfo.Mailboxes = append(accountInfo.Mailboxes, Mailbox{
+			Name:        mailbox.Mailbox,
+			NumMessages: int(*mailbox.Status.NumMessages),
+			NumUnread:   int(*mailbox.Status.NumUnseen),
+		})
+	}
+
+	return accountInfo, nil
 }
--- services/email/email.go
+++ services/email/email.go
@@ -2,21 +2,29 @@ package email
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/piquel-fr/api/config"
 	"github.com/piquel-fr/api/database/repository"
 )
 
 type EmailService interface {
 	// account stuff
 	GetAccountByEmail(ctx context.Context, email string) (repository.MailAccount, error)
-	ListAccounts(ctx context.Context, userId int32) ([]MailAccount, error)
+	ListAccounts(ctx context.Context, userId int32) ([]repository.MailAccount, error)
 	AddAccount(ctx context.Context, params repository.AddEmailAccountParams) (int32, error)
 	RemoveAccount(ctx context.Context, accountId int32) error
 	GetAccountInfo(ctx context.Context, account *repository.MailAccount) (AccountInfo, error)
 }
 
-type realEmailService struct{}
+type realEmailService struct {
+	imapAddr string
+}
 
 func NewRealEmailService() *realEmailService {
-	return &realEmailService{}
+	addr := fmt.Sprintf("%s:%s", config.Envs.ImapHost, config.Envs.ImapPort)
+
+	return &realEmailService{
+		imapAddr: addr,
+	}
 }
