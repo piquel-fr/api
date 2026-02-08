f2d20e1cdfc3612db4cef98a9f1130e4cec1b583
PiquelChips <piquel@piquel.fr>
2026-02-08 16:46:22 +0100

    setup session queries

---
+++ database/queries/sessions.sql
@@ -0,0 +1,18 @@
+-- name: AddSession :one
+INSERT INTO "user_sessions" ("userId", "tokenHash", "userAgent", "ipAdress", "expiresAt")
+VALUES ($1, $2, $3, $4, $5) RETURNING *;
+
+-- name: GetSessionFromHash :one
+SELECT * FROM "user_sessions" WHERE "tokenHash" = $1;
+
+-- name: UpdateSession :exec
+UPDATE "user_sessions" SET "tokenHash" = $2, "expiresAt" = $3 WHERE "userId" = $1;
+
+-- name: DeleteSession :exec
+DELETE FROM "user_sessions" WHERE "id" = $1;
+
+-- name: GetUserSessions :many
+SELECT * FROM "user_sessions" WHERE "userId" = $1 ORDER BY "id" ASC;
+
+-- name: ClearUserSessions :exec
+DELETE FROM "user_sessions" WHERE "userId" = $1;
